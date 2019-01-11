0ca8cc4b474fcc2c099a12ae2a42f8f0935ff538
mcaputolan1 <46543422+mcaputolan1@users.noreply.github.com>
2019-01-11 16:13:44 -0700

    Update Die.cs

--- OOPReviewSolution/OOPReview/Die.cs
+++ OOPReviewSolution/OOPReview/Die.cs
@@ -21,6 +21,7 @@ namespace OOPReview
         //   by the user
 
         private int _Size;
+        private string _Color;
 
         //Properties
         //a property is an external interface between the user
@@ -44,9 +45,53 @@ namespace OOPReview
                 //  the internal private data member
                 //the incoming piece of data is placed into a special
                 //  variable that is called: value
-                _Size = value;
+                //optionally you may place validation on the incoming
+                //  value
+                if (value >= 6 && value <= 20)
+                {
+                    _Size = value;
+                }
+                else
+                {
+                    throw new Exception("Die cannot be " + value.ToString() + " sides. Die must have between 6 and 20 sides.");
+                }
+                
             }
         }
+
+        //Auto Implemented Property
+        // public
+        // It has a data type
+        // It has a name
+        // It does not have an internal data member that you can directly access
+        //The system will create internally, a data storage area of the appropriate
+        // data type and manage the area
+        //The only wat to access the data of an Auto Implemented Property is via
+        // the property
+        //usually use when there is no need for any internal validation  or other property logic
+        public int FaceValue { get; set; }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                // (value == null) This will fail for an empty string
+                // (value == "") This will fail for a null value
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Color has no value.");
+                }
+                else
+                {
+                    _Color = value;
+                }
+            }
+        }
+
         //Constructors
 
         //Behaviors (methods)
